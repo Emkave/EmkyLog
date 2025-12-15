0a6daaacff8b53501726cf76b67431450506f78b
Emkave <natik.agaev.emkave@gmail.com>
2025-12-15 10:04:22 +0100

    new features

--- EmkyLog.h
+++ EmkyLog.h
@@ -18,6 +18,7 @@ class emkylog {
     static std::ofstream log_stream;
     static std::ofstream error_log_stream;
     static bool inited;
+    static bool auto_newline;
 
 public:
     emkylog() = default;
@@ -32,6 +33,8 @@ public:
     static unsigned short SetLogFilename(std::string_view) noexcept;
     static unsigned short set_error_log_filename(std::string_view) noexcept;
     static unsigned short SetErrorLogFilename(std::string_view) noexcept;
+    static void set_auto_new_line(bool &&) noexcept;
+    static void SetAutoNewLine(bool &&) noexcept;
     static std::string_view get_log_path() noexcept;
     static std::string_view GetLogPath() noexcept;
     static std::string_view get_error_log_path() noexcept;
@@ -40,10 +43,12 @@ public:
     static std::string_view GetLogFilename() noexcept;
     static std::string_view get_error_log_filename() noexcept;
     static std::string_view GetErrorLogFilename() noexcept;
-    static unsigned short log(std::string_view);
-    static unsigned short Log(std::string_view);
-    static unsigned short log_error(std::string_view);
-    static unsigned short LogError(std::string_view);
+    static bool get_auto_new_line() noexcept;
+    static bool GetAutoNewLine() noexcept;
+    static unsigned short log(std::string_view, bool new_line=emkylog::auto_newline);
+    static unsigned short Log(std::string_view, bool new_line=emkylog::auto_newline);
+    static unsigned short log_error(std::string_view, bool new_line=emkylog::auto_newline);
+    static unsigned short LogError(std::string_view, bool new_line=emkylog::auto_newline);
     static unsigned short open_logger();
     static unsigned short OpenLogger();
     static unsigned short open_error_logger();
@@ -65,7 +70,7 @@ private:
         level lvl;
 
         static unsigned short flush(const level lvl, const std::string_view str) {
-            return (lvl == level::info) ? emkylog::log(str) : emkylog::log_error(str);
+            return (lvl == level::info) ? emkylog::log(str, true) : emkylog::log_error(str, true);
         }
 
     public:
@@ -79,15 +84,20 @@ private:
             return *this;
         }
 
+        line & operator << (const char ch) {
+            this->string.push_back(ch);
+            return *this;
+        }
+
         line & operator << (const char * s) {
-            return *this << std::string_view{s};
+            return *this << std::string_view(s);
         }
     };
 
     struct stream {
         level lvl;
         line operator << (const std::string_view s) const {
-            line l{lvl};
+            line l(lvl);
             l << s;
             return l;
         }
@@ -104,17 +114,36 @@ private:
     };
 
 public:
-    static constexpr stream loginfo{level::info};
-    static constexpr stream logerror{level::error};
+    static constexpr stream loginfo {level::info};
+    static constexpr stream logerror {level::error};
 };
 
-inline std::string emkylog::log_path = ".";
-inline std::string emkylog::error_log_path = ".";
+inline std::string emkylog::log_path = (std::filesystem::current_path() / "emkylog").string();
+inline std::string emkylog::error_log_path = (std::filesystem::current_path() / "emkylog").string();
 inline std::string emkylog::log_filename = "emkylog.txt";
 inline std::string emkylog::error_log_filename = "emkyerrlog.txt";
 inline std::ofstream emkylog::log_stream = {};
 inline std::ofstream emkylog::error_log_stream = {};
 inline bool emkylog::inited = false;
+inline bool emkylog::auto_newline = true;
+
+
+inline unsigned short emkylog::Init() {return emkylog::init();}
+inline unsigned short emkylog::SetLogPath(const std::string_view path) {return emkylog::set_log_path(path);}
+inline unsigned short emkylog::SetErrorLogPath(const std::string_view path) {return emkylog::set_error_log_path(path);}
+inline unsigned short emkylog::SetLogFilename(const std::string_view filename) noexcept {return emkylog::set_log_filename(filename);}
+inline unsigned short emkylog::SetErrorLogFilename(const std::string_view filename) noexcept {return emkylog::set_error_log_filename(filename);}
+inline void emkylog::SetAutoNewLine(bool && boolean) noexcept {return emkylog::set_auto_new_line(static_cast<bool&&>(boolean));}
+inline std::string_view emkylog::GetLogPath() noexcept {return emkylog::get_log_path();}
+inline std::string_view emkylog::GetErrorLogPath() noexcept {return emkylog::get_error_log_path();}
+inline std::string_view emkylog::GetLogFilename() noexcept {return emkylog::get_log_filename();}
+inline std::string_view emkylog::GetErrorLogFilename() noexcept {return emkylog::get_error_log_filename();}
+inline bool emkylog::GetAutoNewLine() noexcept {return emkylog::get_auto_new_line();}
+inline unsigned short emkylog::Log(const std::string_view log, const bool new_line) {return emkylog::log(log, new_line);}
+inline unsigned short emkylog::LogError(const std::string_view log, const bool new_line) {return emkylog::log_error(log, new_line);}
+inline unsigned short emkylog::OpenLogger() {return emkylog::open_logger();}
+inline unsigned short emkylog::CloseLogger() {return emkylog::close_logger();}
+inline bool emkylog::Initiated() noexcept {return emkylog::initiated();}
 
 
 inline unsigned short emkylog::init() {
@@ -205,6 +234,11 @@ inline unsigned short emkylog::set_error_log_filename(const std::string_view fil
 }
 
 
+inline void emkylog::set_auto_new_line(bool && boolean) noexcept {
+    emkylog::auto_newline = boolean;
+}
+
+
 inline std::string_view emkylog::get_log_path() noexcept {
     return emkylog::log_path;
 }
@@ -225,7 +259,12 @@ inline std::string_view emkylog::get_error_log_filename() noexcept {
 }
 
 
-inline unsigned short emkylog::log(const std::string_view slog) {
+inline bool emkylog::get_auto_new_line() noexcept {
+    return emkylog::auto_newline;
+}
+
+
+inline unsigned short emkylog::log(const std::string_view slog, const bool new_line) {
     if (!emkylog::initiated()) {
         if (const unsigned short res = emkylog::init()) {
             return res;
@@ -240,14 +279,14 @@ inline unsigned short emkylog::log(const std::string_view slog) {
         }
     }
 
-    emkylog::log_stream << slog << '\n';
+    emkylog::log_stream << slog << (new_line ? "\n" : "");
     emkylog::log_stream.flush();
 
     return 0;
 }
 
 
-inline unsigned short emkylog::log_error(const std::string_view slog) {
+inline unsigned short emkylog::log_error(const std::string_view slog, const bool new_line) {
     if (!emkylog::initiated()) {
         if (const unsigned short res = emkylog::init()) {
             return res;
@@ -262,7 +301,7 @@ inline unsigned short emkylog::log_error(const std::string_view slog) {
         }
     }
 
-    emkylog::error_log_stream << slog << '\n';
+    emkylog::error_log_stream << slog << (new_line ? "\n" : "");
     emkylog::error_log_stream.flush();
 
     return 0;
